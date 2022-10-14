08d31c9f860c79b9d94b455acd820c24c655e959
Miguel A. Segovia Freeman. <miguelsegoviafreeman@gmail.com>
2022-10-14 20:06:15 +0200

    Lamina añadida?

--- di2223/src/di2223/CalculadoraSwing.java
+++ di2223/src/di2223/CalculadoraSwing.java
@@ -1,5 +1,6 @@
 package di2223;
 
+import java.awt.*;
 import javax.swing.*;
 
 public class CalculadoraSwing {
@@ -10,17 +11,47 @@ public class CalculadoraSwing {
 		marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
+
+class Lamina extends JPanel{
+	
+	private static final long serialVersionUID = -2495012887631989312L;
+	
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+		
+		g.drawString("Calculando????????",100, 100);
+	}
+}
+
 class Marco extends JFrame{
-	/**
-	 * Nose pa que sirve esto.
-	 */
-	private static final long serialVersionUID = 1L;
+	
+	private static final long serialVersionUID = 2894371221444677478L;
 
 	public Marco() {
+
 		//setSize(500, 300);
 		setBounds(500,300, 600, 600);
 		//setExtendedState(Frame.MAXIMIZED_BOTH);
 		
+		Toolkit pantalla=Toolkit.getDefaultToolkit();
+		Dimension tamañoPantalla=pantalla.getScreenSize();
+		
+		int altura=tamañoPantalla.height;
+		int anchoPantalla=tamañoPantalla.width;
+		
+		setSize(anchoPantalla/2, altura/2);
+		
+		setLocation(anchoPantalla/4,altura/4);
+		
 		setTitle("Calculadora");
+		
+		Image icono=pantalla.getImage("https://p.kindpng.com/picc/s/107-1075239_pokeball-pixel-png-png-download-pokeball-pixel-art.png");
+		
+		setIconImage(icono);
+		
+		Lamina unalamina=new Lamina();
+		
+		add(unalamina);
 	}
 }
